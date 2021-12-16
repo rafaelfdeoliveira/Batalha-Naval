9fc8477cfd4fe3f5a15274bc2e034178a4545066
rafaelfdeoliveira <raf100guy@gmail.com>
2021-12-15 23:09:34 -0300

    Simplified the game methods

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -3,23 +3,24 @@ package Game;
 import java.util.Scanner;
 
 public class Game {
-    public static void main (HumanPlayer humanPlayer, ComputerPlayer computerPlayer, Board humanBoard, Board computerBoard) {
-        Player currentPlayer = getStartingPlayer(humanPlayer, computerPlayer);
-        Player opponentPlayer = getOpponentPlayer(currentPlayer, humanPlayer, computerPlayer);
-        boolean gameOver = false;
-        while (!gameOver) {
-            boolean success = false;
-            while (!success) {
-                String shot = currentPlayer.shoot();
-                success = currentPlayer.board.tryRegisterShot(opponentPlayer.board);
-            }
-            gameOver = currentPlayer.board.hasWon();
-            currentPlayer = getOpponentPlayer(currentPlayer, humanPlayer, computerPlayer);
-            opponentPlayer = getOpponentPlayer(currentPlayer, humanPlayer, computerPlayer);
-        }
+    public static void Game () {
+        Player humanPlayer = new Player();
+        ComputerPlayer computerPlayer = new ComputerPlayer();
+        Ship[] fleet= getFleet();
+        humanPlayer.placeShips(fleet);
+        computerPlayer.placeShips(fleet);
+        playGame(humanPlayer, computerPlayer);
     }
 
-    private static Player getStartingPlayer (HumanPlayer humanPlayer, ComputerPlayer computerPlayer) {
+    private static Ship[] getFleet () {
+        Ship[] fleet = new Ship[10];
+        for (int i = 0; i < fleet.length; i++) {
+            fleet[i] = new Submarine();
+        }
+        return fleet;
+    }
+
+    private static Player getStartingPlayer (Player humanPlayer, ComputerPlayer computerPlayer) {
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.printf("Who starts the game, %s? (human or computer): ", humanPlayer.name);
@@ -30,8 +31,21 @@ public class Game {
         }
     }
 
-    private static Player getOpponentPlayer (Player currentPlayer, HumanPlayer humanPlayer, ComputerPlayer computerPlayer) {
-        if (currentPlayer == humanPlayer) return computerPlayer;
+    private static Player getOtherPlayer (Player player, Player humanPlayer, ComputerPlayer computerPlayer) {
+        if (player == humanPlayer) return computerPlayer;
         return humanPlayer;
+    }
+
+    private static void playGame (Player humanPlayer, ComputerPlayer computerPlayer) {
+        Player currentPlayer = getStartingPlayer(humanPlayer, computerPlayer);
+        Player opponentPlayer = getOtherPlayer(currentPlayer, humanPlayer, computerPlayer);
+        boolean gameOver = false;
+        while (!gameOver) {
+            String shot = currentPlayer.shoot();
+            opponentPlayer.board.getOpponentShot(shot);
+            gameOver = currentPlayer.board.hasWon();
+            opponentPlayer = currentPlayer;
+            currentPlayer = getOtherPlayer(currentPlayer, humanPlayer, computerPlayer);
+        }
     }
 }
