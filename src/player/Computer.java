89f2ff16d7a44f2de37102a183b7b1bb5bef59b7
MoraesArmando <armando-am16@hotmail.com>
2021-12-17 17:40:57 -0300

    Changing the Player class, Refactoring attributes and methods, Adding the constructor method.

--- src/player/Computer.java
+++ src/player/Computer.java
@@ -1,33 +1,38 @@
 package player;
 
 import java.util.Locale;
-import java.util.Random;
 
-public class Computer implements Player {
-    Random random = new Random();
+public class Computer extends Player {
 
     private final String name = "Computer";
-    private String[] shot = new String[2];
-    private String[] ship = new  String[2];
-    private String shotList;
 
-    public void setShip() {
+    /*
+    public Computer(String name, List<Ship> ships, Board board) {
+        super(name, ships, board);
+    }
+    */
+    @Override
+    public String[] setShip() {
         String arrayLetter = "ABCDEFGHIJ";
         String impPlayer = (String) (String.valueOf(arrayLetter.charAt(random.nextInt(arrayLetter.length()))) + (random.nextInt(9) + 1));
         //Excluir Linha
         System.out.println(impPlayer);
-        this.ship = setArrayShot(impPlayer);
+        String[] ship = setArrayShot(impPlayer);
+        return ship;
     }
 
-    public void setShot() {
+    @Override
+    public String[] setShot() {
         String arrayLetter = "ABCDEFGHIJ";
         String impPlayer = (String) (String.valueOf(arrayLetter.charAt(random.nextInt(arrayLetter.length()))) + (random.nextInt(9) + 1));
         //Excluir Linha
         System.out.println(impPlayer);
-        this.shot = setArrayShot(impPlayer);
+        String[] shot = setArrayShot(impPlayer);
+        return shot;
     }
 
     //TRATAMENTO DA ENTRADA DO JOGADOR
+    @
     private String[] setArrayShot(String impPlayer) {
         String[] arrayShot = new String[2];
         for (int i = 0; i < arrayShot.length; i++) {
@@ -36,8 +41,5 @@ public class Computer implements Player {
         return arrayShot;
     }
 
-    public String[] getShot() {
-        return shot;
-    }
 
 }
--- src/player/Human.java
+++ src/player/Human.java
@@ -3,42 +3,10 @@ package player;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Human implements Player {
-    Scanner sc = new Scanner(System.in);
-
-    private String name;
-    private String[] shot = new String[2];
-    private String[] ship = new  String[2];
-    private String[] shotList;
-
-    public void setShip() {
-        System.out.println("Pronto para posicionar seu navio...");
-        System.out.print("Informe uma linha e uma coluna (Ex: A1):");
-        String impPlayer = this.sc.nextLine();
-        //Excluir Linha
-        System.out.println(impPlayer.toUpperCase(Locale.ROOT));
-        this.ship = setArrayShot(impPlayer);
+public class Human extends Player {
+    /*
+    public Human(String name, List<Ship> ships, Board board) {
+        super(name, ships, board);
     }
-
-    public void setShot() {
-        System.out.println("Pronto para fazer sua jogada...");
-        System.out.print("Informe uma linha e uma coluna (Ex: A1):");
-        String impPlayer = this.sc.nextLine();
-        //Excluir Linha
-        System.out.println(impPlayer.toUpperCase(Locale.ROOT));
-        this.shot = setArrayShot(impPlayer);
-    }
-
-    //TRATAMENTO DA ENTRADA DO JOGADOR
-    private String[] setArrayShot(String impPlayer) {
-        String[] arrayShot = new String[2];
-        for (int i = 0; i < arrayShot.length; i++) {
-            arrayShot[i] = impPlayer.toUpperCase(Locale.ROOT).substring(i, i + 1);
-        }
-        return arrayShot;
-    }
-
-    public String[] getShot() {
-        return shot;
-    }
+    */
 }
