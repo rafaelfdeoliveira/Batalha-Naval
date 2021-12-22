f3721ecc7154df089e1b3e3438832e0b23ba98af
MoraesArmando <armando-am16@hotmail.com>
2021-12-22 13:45:56 -0300

    Changing the Player class, Refactoring attributes and methods, Adding the constructor method.

--- src/root/Main.java
+++ src/root/Main.java
@@ -1,18 +1,14 @@
 package root;
 
 import player.Computer;
-import player.Human;
 
 public class Main {
 
     public static void main(String[] args) {
-        Human human = new Human();
         Computer computer = new Computer();
 
-        human.setShip();
         computer.setShip();
 
-        human.setShot();
         computer.setShot();
     }
 }
